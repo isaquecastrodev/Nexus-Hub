15c5565f4345cb68fdfc4e0c5c18a99cdb2f8c80
IsaqueDev <78563248+isaquecastrodev@users.noreply.github.com>
2025-11-15 22:53:29 -0300

    Create Settings.lua

---
+++ ServerScriptService/NexusCore/Settings.lua
@@ -0,0 +1,12 @@
+return {
+    Ranks = {
+        Owner = {ID = 123456, Color = "rainbow"},
+        ["Sub-Owner"] = {ID = 123457, Color = "rainbow"},
+        Admin = {ID = 123458, Color = Color3.fromRGB(255, 0, 0)},
+        Moderator = {ID = 123459, Color = Color3.fromRGB(0, 255, 0)}
+    },
+    AntiCheat = {
+        MaxSpeed = 50,
+        MaxJump = 50
+    }
+}
